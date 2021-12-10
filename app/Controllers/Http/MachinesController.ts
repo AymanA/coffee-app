24cb8bd6d62b26649adf07bdeb354b635d037573
aymanfahmy <aymanfahmy@coral.io>
2021-12-10 08:35:31 +0200

    machines logic

--- app/Controllers/Http/MachinesController.ts
+++ app/Controllers/Http/MachinesController.ts
@@ -1,3 +1,19 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import MachineService from 'App/services/MachineService';
 
-export default class MachinesController {}
+export default class MachinesController {
+    private machineService;
+    constructor() {
+        this.machineService = new MachineService()
+    }
+
+    public async getMachines({ request, response }: HttpContextContract) {
+        const data = request.qs()
+        const machines = await this.machineService.getMachines(data)
+        
+        return response.json({
+            data: machines
+        });
+    }
+
+}
